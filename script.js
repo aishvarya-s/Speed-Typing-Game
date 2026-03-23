a3c054f0b4300f7f1c3d06b0c1c82891021742bd
aishvarya-s <aishvaryasudha@gmail.com>
2026-03-23 21:44:38 +0530

    Added blur and distortion effects

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const textDisplay = document.getElementById("text");
 const input = document.getElementById("input");
 
+let idleTimer; 
+
 // sample text
 const text = "The quick brown fox jumps over the lazy dog";
 
@@ -15,8 +17,37 @@ text.split("").forEach(char => {
 
 const spans = textDisplay.querySelectorAll("span");
 
+function triggerShake() {
+  const game = document.getElementById("game");
+
+  game.classList.add("shake");
+
+  setTimeout(() => {
+    game.classList.remove("shake");
+  }, 200);
+}
+
 input.addEventListener("input", () => {
+
+  // remove drift when user types
+  document.body.classList.remove("drift");
+
+  // reset timer
+  clearTimeout(idleTimer);
+
+  // start timer again
+  idleTimer = setTimeout(() => {
+    document.body.classList.add("drift");
+  }, 1500); // 1.5 seconds
+
+  
   const typed = input.value.split("");
+
+  const currentIndex = typed.length - 1;
+
+  if (currentIndex >= 0 && typed[currentIndex] !== text[currentIndex]) {
+  triggerShake();
+  }
 
   spans.forEach((span, index) => {
     const char = typed[index];
